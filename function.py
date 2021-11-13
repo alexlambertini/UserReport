c48488e5bce6fdc7a47cacd7004b8ff1b3b05291
claudiohickstein <claudiohick@gmail.com>
2021-11-13 13:07:23 -0300

    updating output consultarDados()

--- function.py
+++ function.py
@@ -31,14 +31,29 @@ def consultarDados(date):
                 file_data = 'registro-{}.txt'.format(date)
 
                 # for dados in sql:
-                #         print(dados)
+                print(sql[0][0])
+
                 with open(file_data, 'w') as file:             
+                        # apenas para registro - inicio da escrita
                         print('Beign writing {} to txt'.format(sql))
+                        # --- organizando os dados
+                        # --- muito cuidado, pois esta sempre sobrescrevendo no dia.
+                        # 
                         file.write(str(sql))
+                        # para debug apenas
+                        print("Para nome -> {}".format(sql[0][0]))
+                        print("Para email -> {}".format(sql[0][1]))
+                        print("Para ddd -> {}".format(sql[0][2]))
+                        print("Para telefone -> {}".format(sql[0][3]))
+                        print("Para report -> {}".format(sql[0][4]))
+                        print("Para data -> {}".format(sql[0][5]))
+
+                        # fim da escrita.
                         print('End writing {} to txt'.format(sql))
                         file.flush()
-
         except Exception as err:
                 print(err)
                 ShowPopup("Alerta","Não foi possível consultar dados")
-consultarDados('2021-11-13')
+
+if __name__ == "__main__":
+        consultarDados('2021-11-13')
