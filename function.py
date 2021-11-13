ce66a4f9daa8c6b3b8fea63f0e8fb4b88967ed93
Alexandre Lambertini <38925045+alexlambertini@users.noreply.github.com>
2021-11-13 02:10:02 -0300

    refresh

--- function.py
+++ function.py
@@ -1,13 +1,36 @@
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLineEdit, QInputDialog, QMessageBox)
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5 import QtGui
+import sqlite3
+
+def ShowPopup(titulo, mensagem):
+        print("não foi")
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setWindowIcon(QtGui.QIcon('ico/relatorio.ico'))
+        msgBox.setWindowTitle(titulo)
+        msgBox.setText(mensagem)
+        msgBox.setStandardButtons(QMessageBox.Ok)
+   
+        msgBox.exec_()
 
 
-if __name__ == "__main__":
-        import sys
-        app = QtWidgets.QApplication(sys.argv)
-        Principal = QtWidgets.QMainWindow()
-        ui = Ui_MainWindow()
-        ui.setupUi(Principal)
-        Principal.show()
-        sys.exit(app.exec_())
+def consultarDados():
+        try:
+                banco = sqlite3.connect('relatorios.db')
+                cursor = banco.cursor()
+                cursor.execute("SELECT * FROM dados")
 
+                sql = cursor.fetchall()
+
+                ''''
+                pessoas = [({'nome': '?', 'email': '?', 'DD': '?', 'telefone': '?', 'caso': '?', 'data': '?'}),
+
+                for dados in sql:
+                   with open('relatorio.txt', 'w') as f:
+                      f.writelines(dados)
+                      f.write('\n')
+                '''
+
+
+        except:
+                ShowPopup("Alerta","Não foi possível consultar dados")
