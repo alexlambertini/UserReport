482c556023e4768bf23a261e53f93a59b10a5c9a
claudiohickstein <claudiohick@gmail.com>
2021-11-15 23:51:47 -0300

    new report

--- function.py
+++ function.py
@@ -40,41 +40,18 @@ def consultarDados(date):
                 # --- organizando os dados
                 # --- muito cuidado, pois esta sempre sobrescrevendo no dia.
                 # 
-                # file.write(str(sql))
-                # report = None
                 tmp = []
                 file = open(file_data, 'w')
                         
                 for data in range(0, len(sql)):
                         # print(data[4])
                         nome = sql[data][0]
-                        # email = data[0][1]
-                        # ddd = data[0][2]
-                        # telefone = data[0][3]
                         mensagem = sql[data][4]
-                        # data = data[0][5]
                         # # build report
                         report = 'Nome: {}\n'.format(nome)
                         report += 'Mensagem: {}'.format(mensagem)
-                        #tmp.append("Nome: {}\nEmail: {}\nTelefone: {}\n".format(
-                        #        nome, 
-                        #        email, 
-                        #        telefone))
-                        # report += "Email: {}\n".format(email)
                         print(str(report))
                         file.write(str(report))
-                                
-        
-                # para debug apenas
-                # print("Para nome -> {}".format(data[0][0]))
-                # print("Para email -> {}".format(data[0][1]))
-                # print("Para ddd -> {}".format(data[0][2]))
-                # print("Para telefone -> {}".format(data[0][3]))
-                # print("Para mensagem -> {}".format(data[0][4]))
-                # print("Para data -> {}".format(data[0][5]))
-
-                # fim da escrita.
-                # print('End writing {} to txt'.format(sql))
                 file.flush()
         except Exception as err:
                 print(err)
